06eb237d49f291fe22630c981ece00bd2e6f9061
Rune Warhuus <rune@dinkdonk.no>
2013-11-18 02:43:09 +0100

    Initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,7 @@
+'use strict';
+
 module.exports = function(grunt) {
-	grunt.registerMultiTask('csstojavascript', 'Convert CSS file to JavaScript file', function() {
-		//var opts = this.options();
+	grunt.initConfig({});
 
-		this.files.forEach(function(f) {
-			grunt.log.writeln('got a file!');
-			/*var ret = f.src.map(function(srcFile) {
-				var result = task(grunt.file.read(srcFile), opts);
-				grunt.log.writeln("Removed " + result.count + " logging statements from " + srcFile);
-				return result.src;
-			}).join("");
-
-			grunt.file.write(f.dest, ret);*/
-		});
-	});
+	grunt.loadTasks('tasks');
 };
---
+++ tasks/csstojavascript.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.registerMultiTask('csstojavascript', 'Convert CSS file to JavaScript file', function() {
+		var options = this.options({});
+
+		this.files.forEach(function(f) {
+			grunt.log.writeln('got a file!');
+			/*var ret = f.src.map(function(srcFile) {
+				var result = task(grunt.file.read(srcFile), opts);
+				grunt.log.writeln("Removed " + result.count + " logging statements from " + srcFile);
+				return result.src;
+			}).join("");
+
+			grunt.file.write(f.dest, ret);*/
+		});
+	});
+};
